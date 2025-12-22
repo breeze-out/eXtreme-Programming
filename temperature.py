c561304c3dde5fbd98269407686484f80202f706
breeze-out <coreterea@gmail.com>
2025-12-22 13:08:22 +0400

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,19 @@
+def celsius_to_fahrenheit(celsius):
+
+
+# Конвертирует температуру из градусов Цельсия в градусы Фаренгейта
+return (celsius * 9 / 5) + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+
+
+# Конвертирует температуру из градусов Фаренгейта в градусы Цельсия
+return (fahrenheit - 32) * 5 / 9
+
+
+def celsius_to_kelvin(celsius):
+
+
+# Конвертирует температуру из градусов Цельсия в Кельвины
+return celsius + 273.15
